d3946e492f487c5d5243762a0b6ffae8d2166466
paphat-82 <paphat82@gmail.com>
2024-08-16 15:37:24 +0700

    Update app.js v45

--- app.js
+++ app.js
@@ -41,8 +41,8 @@ const server = http.createServer((req, res) => {
       </style>
     </head>
     <body>
-      <h1>WELCOME THAILAND:V44</h1>
-      <img src="https://images.pexels.com/photos/4321108/pexels-photo-4321108.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Beautiful Thailand">
+      <h1>WELCOME THAILAND:V45/h1>
+      <img src="https://images.pexels.com/photos/4319838/pexels-photo-4319838.jpeg" alt="Beautiful Thailand">
     </body>
     </html>
   `);
